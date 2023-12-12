583a01339821d89fb66c66417705cd610031d450
Zahid Hasan <zahidhasan140338@gmail.com>
2023-12-12 23:37:10 +0600

    basic function and parameters and arguments has been learned

---
+++ parametersAndArguments.js
@@ -0,0 +1,9 @@
+// arameters are variables that act as placeholders for the values that are to be input to a function when it is called. When a function is defined, it is typically defined along with one or more parameters. The actual values that are input (or "passed") into a function when it is called are known as arguments.
+
+
+
+function functionWithArgs(param1, param2) {
+    console.log(param1+param2);
+  }
+  
+  functionWithArgs(2,3)
