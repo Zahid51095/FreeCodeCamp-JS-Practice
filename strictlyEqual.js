8f8977d9a63a735eeb31c505cf1f8878a3150f87
Zahid Hasan <zahidhasan140338@gmail.com>
2023-12-13 15:19:57 +0600

    learned about equal and strictly equal

---
+++ strictlyEqual.js
@@ -0,0 +1,12 @@
+// In the last two challenges, we learned about the equality operator (==) and the strict equality operator (===). Let's do a quick review and practice using these operators some more.
+
+// If the values being compared are not of the same type, the equality operator will perform a type conversion, and then evaluate the values. However, the strict equality operator will compare both the data type and value as-is, without converting one type to the other.
+
+function compareEquality(a, b) {
+    if (a === b) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  compareEquality(10, "10");
