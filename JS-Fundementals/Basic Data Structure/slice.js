b87f4e44a99a58d4b785c859cb250d91038510a0
Zahid Hasan <zahidhasan140338@gmail.com>
2023-12-26 23:00:53 +0600

    learned to copy the items of an array

---
+++ JS-Fundementals/Basic Data Structure/slice.js
@@ -0,0 +1,7 @@
+function forecast(arr) {
+    const newArray = arr.slice(2, 4)
+    return newArray;
+  }
+  
+  
+  console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
