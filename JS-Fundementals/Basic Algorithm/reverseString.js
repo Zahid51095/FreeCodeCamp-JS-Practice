7815350d0ece0bd3183a00973cdd355b05527f92
Zahid Hasan <zahidhasan140338@gmail.com>
2023-12-30 22:45:47 +0600

    created a function to reverse a string

---
+++ JS-Fundementals/Basic Algorithm/reverseString.js
@@ -0,0 +1,11 @@
+function reverseString(str) {
+    let reversedString = "";
+    for(let i = str.length - 1;i >= 0; i--){
+      reversedString += str[i];
+    }
+    return reversedString;
+  }
+  
+  const ans = reverseString("hello");
+  console.log(ans)
+  
