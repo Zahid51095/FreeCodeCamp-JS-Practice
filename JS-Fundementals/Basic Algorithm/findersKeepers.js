04bf5bb03c32a592b13f8e9bf84799063276e08c
Zahid Hasan <zahidhasan140338@gmail.com>
2024-01-03 22:41:57 +0600

    solved the problem finders keepers

---
+++ JS-Fundementals/Basic Algorithm/findersKeepers.js
@@ -0,0 +1,13 @@
+function findElement(arr, func) {
+    let num = 0;
+    for(let i = 0; i < arr.length; i++){
+      num = arr[i];
+      if(func(num)){
+        return num
+      }
+    }
+    return num;
+  }
+  
+  const ans = findElement([1, 2, 3, 4], num => num % 2 === 0);
+  console.log(ans)
