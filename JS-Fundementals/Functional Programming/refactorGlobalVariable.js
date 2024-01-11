53537578b4bdd61abff709e782a428fab271407f
Zahid Hasan <zahidhasan140338@gmail.com>
2024-01-11 13:52:26 +0600

    learned to Refactor Global Variables Out of Functions~

---
+++ JS-Fundementals/Functional Programming/refactorGlobalVariable.js
@@ -0,0 +1,21 @@
+// The global variable
+const bookList = [
+  "The Hound of the Baskervilles",
+  "On The Electrodynamics of Moving Bodies",
+  "Philosophiæ Naturalis Principia Mathematica",
+  "Disquisitiones Arithmeticae",
+];
+
+function add(arr, bookName) {
+  let newArr = [...arr];
+  newArr.push(bookName);
+  return newArr;
+}
+
+function remove(arr, bookName) {
+  let newArr = [...arr];
+  if (newArr.indexOf(bookName) >= 0) {
+    newArr.splice(newArr.indexOf(bookName), 1);
+    return newArr;
+  }
+}
